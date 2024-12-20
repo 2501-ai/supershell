66826dcaca7cb0a190f43471f299e2bac28f51d3
Sebastien Hideux <hideux.sebastien@gmail.com>
2024-12-20 10:28:22 +0100

    feat: commented logs

--- shell/common.sh
+++ shell/common.sh
@@ -4,21 +4,21 @@
 info "[COMMON] Initializing common shell functions..."
 
 _universal_complete() {
-    info "[COMMON] Universal complete triggered"
+    # info "[COMMON] Universal complete triggered"
     local current_word="$1"
-    local cursor_pos="$2"
+    # local cursor_pos="$2"
     
-    info "[COMMON] Current word: $current_word"
-    info "[COMMON] Cursor position: $cursor_pos"
+    # info "[COMMON] Current word: $current_word"
+    # info "[COMMON] Cursor position: $cursor_pos"
     
     if [ -z "$current_word" ]; then
-        info "[COMMON] Empty input, clearing display"
+        # info "[COMMON] Empty input, clearing display"
         clear_lines
         return
     fi
     
     if [ ${#current_word} -ge 2 ]; then
-        info "[COMMON] Word length sufficient, fetching suggestions"
+        # info "[COMMON] Word length sufficient, fetching suggestions"
         _show_loading
         _debounced_suggest "$current_word"
     fi
