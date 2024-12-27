3d3b17191bb9b9a828e477caf223db6b18358b91
AMGK <maxime.begaud@icloud.com>
2024-12-27 18:56:41 +0900

    Fix for the gap between input and suggestions for apple terminal

--- utils/ansi.sh
+++ utils/ansi.sh
@@ -23,7 +23,13 @@ clear_lines() {
     done
     
     printf '\n\033[K'  # Clear hint line
-    printf '\033[5A'   # Move cursor back up five lines
+
+    if [[ "$TERM_PROGRAM" == "Apple_Terminal" ]]; then
+        printf '\033[6A'  # Clear suggestion line
+    else
+        printf '\033[5A'   # Move cursor back up five lines
+    fi
+    
 }
 
 clear_lines_force() {
